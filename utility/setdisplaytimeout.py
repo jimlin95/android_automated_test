40e8921ae4843e88a76b38697aa92f4a4c564454
Jim Lin <jim_lin@quantatw.com>
2015-05-18 16:45:15 +0800

    Add set display timeout function
    Usage:
    
    from  utility.setdisplaytimeout import setDisplayTimeout
    setDisplayTimeout(d,"30min")
    
    
    choices:
    
      "15sec":"15 seconds",
      "30sec":"30 seconds",
      "1min":"1 minutes",
      "2min":"2 minutes",
      "10min":"10 minutes",
      "30min":"30 minutes"
    
    Signed-off-by: Jim Lin <jim_lin@quantatw.com>

---
+++ utility/setdisplaytimeout.py
@@ -0,0 +1,37 @@
+#! /usr/bin/env python 
+# -*- coding: utf-8 -*-
+import sys
+import os
+from uiautomator import Device
+import utility.common as u
+def setDisplayTimeout(d,timeout):
+    package = 'com.android.settings'
+    activity = '.DisplaySettings'
+    component_name = package + '/' + activity
+    u.start_activity(d,component_name)
+    SLEEP_TIMEOUT = {
+        "15sec":"15 seconds",
+        "30sec":"30 seconds", 
+        "1min":"1 minutes",
+        "2min":"2 minutes",
+        "10min":"10 minutes",
+        "30min":"30 minutes"
+    }
+
+    for k, v in SLEEP_TIMEOUT.iteritems():
+        if k == timeout:
+            ftimeout = v
+            print(ftimeout)
+            break
+
+    d(text=u'Sleep').click()
+    d(text=ftimeout).click()
+
+
+if __name__ == '__main__':
+    d = Device()
+    # Press the HOME button to start the test from the home screen
+    d.press.home()
+    setDisplayTimeout(d,u"30min")
+    # Press the HOME button to start the test from the home screen
+    d.press.home()
